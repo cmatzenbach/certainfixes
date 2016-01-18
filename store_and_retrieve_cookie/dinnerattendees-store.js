5542cb6b77b7927c5f69ec171addffae5c01d90d
Chris Matzenbach <cmatzenbach@gmail.com>
2016-01-18 15:44:13 -0500

    updated core functions

--- store_and_retrieve_cookie/dinnerattendees-store.js
+++ store_and_retrieve_cookie/dinnerattendees-store.js
@@ -2,7 +2,7 @@
 (function ($) {
 
 // Create new object and all properties; set all properties to 0 for now
-attendees = {
+var attendees = {
        fn1:0,
        ln1:0,
        e1:0,
@@ -55,42 +55,33 @@ var questionsArray = [
 var j = 0;
 
 for (i=0; i < dataArray.length; i++) {
-document.getElementById(questionsArray[j]).value = dataArray[i];
-j++;
+       document.getElementById(questionsArray[j]).value = dataArray[i];
+       j++;
 }
 
 $('form').submit(function() { 
+       // Get all entered attendee data
+       (function getStuff (obj, data) {
+	       var i = 0;
+		for (prop in obj) {
+		    if(document.getElementById(data[i])) { obj[prop] = document.getElementById(data[i]).value; }
+		    i++;
+		}
+	 })(attendees, attFields);
 
-            // Get all entered attendee data
-            if(document.getElementById("reg_ans_field04")) { attendees.fn1 = document.getElementById("reg_ans_field04").value; }
-            if(document.getElementById("reg_ans_field05")) { attendees.ln1 = document.getElementById("reg_ans_field05").value; }
-            if(document.getElementById("reg_ans_field06")) { attendees.e1 = document.getElementById("reg_ans_field06").value; }
-            if(document.getElementById("reg_ans_field07")) { attendees.fn2 = document.getElementById("reg_ans_field07").value; }
-            if(document.getElementById("reg_ans_field08")) { attendees.ln2 = document.getElementById("reg_ans_field08").value; }
-            if(document.getElementById("reg_ans_field09")) { attendees.e2 = document.getElementById("reg_ans_field09").value; }
-            if(document.getElementById("reg_ans_field10")) { attendees.fn3 = document.getElementById("reg_ans_field10").value; }
-            if(document.getElementById("reg_ans_field11")) { attendees.ln3 = document.getElementById("reg_ans_field11").value; }
-            if(document.getElementById("reg_ans_field13")) { attendees.e3 = document.getElementById("reg_ans_field13").value; }
-            if(document.getElementById("reg_ans_field12")) { attendees.fn4 = document.getElementById("reg_ans_field12").value; }
-            if(document.getElementById("reg_ans_field14")) { attendees.ln4 = document.getElementById("reg_ans_field14").value; }
-            if(document.getElementById("reg_ans_field15")) { attendees.e4 = document.getElementById("reg_ans_field15").value; }
-            if(document.getElementById("reg_ans_field16")) { attendees.fn5 = document.getElementById("reg_ans_field16").value; }
-            if(document.getElementById("reg_ans_field17")) { attendees.ln5 = document.getElementById("reg_ans_field17").value; }
-            if(document.getElementById("reg_ans_field18")) { attendees.e5 = document.getElementById("reg_ans_field18").value; }
-
-            enteredAttendees = [];  // create new array to store entered (non-zero) data for cookie
-            i = 0;
-            for (var prop in attendees) {
-                 if (attendees[prop] !== 0) {
-                      enteredAttendees[i] = attendees[prop];
-                      i++;
-                 }
-            }
+       enteredAttendees = [];  // create new array to store entered (non-zero) data for cookie
+       i = 0;
+       for (var prop in attendees) {
+          if (attendees[prop] !== 0) {
+               enteredAttendees[i] = attendees[prop];
+               i++;
+          }
+       }
 
 
-            // Store multiple values in same cookie
-            var attdata = enteredAttendees.join(',');
-            document.cookie = "cookiename" + "=" + attdata;
+       // Store multiple values in same cookie
+       var attdata = enteredAttendees.join(',');
+       document.cookie = "cookiename" + "=" + attdata;
 
 });
 
